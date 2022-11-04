9de1d63b5c8aaf5f3d319b64ebc3717ea7ec7ff2
Erick Estrada <eof105@gmail.com>
2022-11-04 11:45:50 -0600

    Mostrar Scans UI - Sin base de datos

--- lib/pages/mapas_page.dart
+++ lib/pages/mapas_page.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+
 class MapasPage extends StatelessWidget {
   const MapasPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('MapasPage'),
+    return ListView.builder(
+      itemCount: 10,
+      itemBuilder: (context, index) => ListTile(
+        leading: Icon(Icons.map, color: Theme.of(context).primaryColor),
+        title: Text('http://'),
+        subtitle: Text('Id: 1'),
+        trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey),
+        onTap: () => print('abre algo'),
+      ),
     );
   }
 }
